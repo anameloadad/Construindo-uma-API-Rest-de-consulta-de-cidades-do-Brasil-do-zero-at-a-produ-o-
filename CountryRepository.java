73502e2556682f7e690d8d0fffda0486753f6f58
anameloadad <86542265+anameloadad@users.noreply.github.com>
2021-06-27 01:01:05 -0300

    Delete CountryRepository.java

--- CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.weverson.countries.repositories;
-
-import com.github.weverson.countries.entities.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
